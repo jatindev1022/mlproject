10f31ba287aff1a38878a58aa248af953272846f
JatinBhardwaj <jatindev1022@gmail.com>
2026-03-21 13:05:06 +0530

    added hyper parameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,7 +18,7 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object,evaluate_model
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -27,48 +27,91 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
-           logging.info("splitting train and test input data")
-           X_train = train_array[:, :-1]
-           y_train = train_array[:, -1]
+            logging.info("splitting train and test input data")
 
-           X_test = test_array[:, :-1]
-           y_test = test_array[:, -1]
-           
-           models={
-               "Random Forest":RandomForestRegressor(),
-               "Decesion tree":DecisionTreeRegressor(),
-               "Gradient Boosting":GradientBoostingRegressor(),
-               "Linear Regressor":LinearRegression(),
-               "k-Neighbors Classifier":KNeighborsRegressor(),
-               "XGBoost regressor":XGBRegressor(),
-               "CatBoosting Classifier":CatBoostRegressor(verbose=False),
-               "AdaBoost Classfier":AdaBoostRegressor(),
-           }
-           
-           model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
-           
-           best_model_score=max(sorted(model_report.values()))
-           
-           best_model_name=list(model_report.keys())[
-               list(model_report.values()).index(best_model_score)
-           ]
-           
-           best_model=models[best_model_name]
-           
-           if best_model_score<0.6:
-               raise CustomException("no nest model found")
-           
-           logging.info("best found model on both traing and testing data set")
-           save_object(
-                    file_path=self.model_trainer_config.trained_model_file_path,
-                    obj=best_model
+            X_train = train_array[:, :-1]
+            y_train = train_array[:, -1]
+
+            X_test = test_array[:, :-1]
+            y_test = test_array[:, -1]
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "KNeighbors": KNeighborsRegressor(),
+                "XGBoost": XGBRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False),
+                "AdaBoost": AdaBoostRegressor(),
+            }
+
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.8, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128]
+                },
+                "Linear Regression": {},
+                "KNeighbors": {},
+                "XGBoost": {
+                    'learning_rate': [.1, .01, .05],
+                    'n_estimators': [8, 16, 32, 64]
+                },
+                "CatBoost": {
+                    'depth': [6, 8],
+                    'learning_rate': [0.01, 0.05],
+                    'iterations': [30, 50]
+                },
+                "AdaBoost": {
+                    'learning_rate': [.1, .01],
+                    'n_estimators': [8, 16, 32]
+                }
+            }
+
+            # 🔥 Evaluate models
+            model_report = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                param=params
             )
-           predicticted=best_model.predict(X_test)
-           r2_square=r2_score(y_test,predicticted)
-           return r2_square
-        except Exception as e:
-            raise CustomException(e,sys)
 
+            # ✅ Get best model
+            best_model_name = max(model_report, key=model_report.get)
+            best_model_score = model_report[best_model_name]
+
+            best_model = models[best_model_name]
+
+            # 🔥 IMPORTANT → train best model again
+            best_model.fit(X_train, y_train)
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+
+            logging.info("Best model found")
+
+            # ✅ Save model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            # ✅ Final prediction
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
